b93b680be81fb27717774f6c07b5e5bbacca53a2
AmirHossein <73388470+amir-hossein-rajaezadeh@users.noreply.github.com>
2024-07-15 16:23:49 +0330

    Working in MainPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_vpn/utils/my_colors.dart';
 import 'package:flutter_vpn/utils/my_strings.dart';
 import 'package:svg_flutter/svg.dart';
@@ -19,7 +20,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter VPN',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -29,10 +30,6 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
-enum UploadDownloadEnum { download, upload }
-
-enum SpeedTypeEnum { mb, kb }
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -67,9 +64,9 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         children: [
           Stack(
             children: [
-              Container(
-                margin: const EdgeInsets.only(top: 20),
-                child: Image.asset("assets/images/earth.png"),
+              Image.asset(
+                "assets/images/earth.png",
+                fit: BoxFit.fitWidth,
               ),
               Container(
                 margin: const EdgeInsets.only(top: 60),
@@ -108,7 +105,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 150),
+                padding: const EdgeInsets.only(top: 130),
                 child: Column(
                   children: [
                     const Center(
@@ -120,19 +117,30 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                             fontWeight: FontWeight.w700),
                       ),
                     ),
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 30),
+                      child: const Text(
+                        "CONNECTED",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Color.fromRGBO(159, 255, 87, 1),
+                        ),
+                      ),
+                    ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Container(
-                          child: _buildUploadDownloadSpeed("124.1",
-                              SpeedTypeEnum.mb, UploadDownloadEnum.download),
+                          child: _buildUploadDownloadSpeed(
+                              "124.1", "Mb/s", "download"),
                         ),
                         const SizedBox(
                           width: 40,
                         ),
                         Container(
-                          child: _buildUploadDownloadSpeed("236.4",
-                              SpeedTypeEnum.kb, UploadDownloadEnum.upload),
+                          child: _buildUploadDownloadSpeed(
+                              "236.4", "Kb/s", "upload"),
                         ),
                       ],
                     )
@@ -141,116 +149,187 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
               )
             ],
           ),
-          Container(
-            margin: const EdgeInsets.only(top: 40),
-            width: 132,
-            height: 274,
-            decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.7),
-              borderRadius: BorderRadius.circular(60),
-              boxShadow: const [
-                BoxShadow(
-                  color: Color.fromRGBO(255, 255, 255, 0.16),
-                  spreadRadius: 3,
-                  blurRadius: 4,
-                  offset: Offset(0, -4),
-                ),
-              ],
-            ),
+          Expanded(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Container(
-                  margin: const EdgeInsets.only(top: 35),
-                  height: 48,
-                  width: 32,
-                  child: SvgPicture.asset(
-                    "assets/icons/arrow_up.svg",
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                SlideTransition(
-                  position: _switchAnimation,
-                  child: GestureDetector(
-                    onTap: () {
-                      if (_switchAnimation.value ==
-                          Offset.zero) {
-                        _switchSlideController.forward();
-                      } else {
-                        _switchSlideController.reverse();
-                      }
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.only(bottom: 8),
-                      width: 108,
-                      height: 158,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        gradient: const LinearGradient(
-                          begin: Alignment.topCenter,
-                          transform: GradientRotation(9.87),
-                          stops: [0.1, 1],
-                          colors: [
-                            Color.fromRGBO(178, 181, 54, 1),
-                            Color.fromRGBO(126, 177, 88, 1),
-                          ],
-                        ),
+                  margin: const EdgeInsets.only(top: 40),
+                  width: 132,
+                  height: 274,
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.7),
+                    borderRadius: BorderRadius.circular(60),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color.fromRGBO(255, 255, 255, 0.16),
+                        spreadRadius: 3,
+                        blurRadius: 4,
+                        offset: Offset(0, -4),
                       ),
-                      child: Container(
-                        margin: const EdgeInsets.only(top: 40),
-                        child: Column(
+                    ],
+                  ),
+                  child: _buildToggleBtn(),
+                ),
+                Container(
+                  height: 60,
+                  margin: const EdgeInsets.only(
+                      top: 20, right: 20, left: 20, bottom: 60),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(60),
+                    gradient: const LinearGradient(
+                      colors: [
+                        Color.fromRGBO(159, 255, 87, 1),
+                        Color.fromRGBO(
+                          195,
+                          252,
+                          151,
+                          1,
+                        )
+                      ],
+                    ),
+                  ),
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 20, left: 12),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
                           children: [
-                            const Text(
-                              MyStrings.stop,
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 14),
-                            ),
                             Container(
-                              margin: const EdgeInsets.only(top: 16),
-                              width: 60,
-                              height: 60,
-                              decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                  begin: Alignment.topCenter,
-                                  stops: const [0, 1],
-                                  end: Alignment.bottomCenter,
-                                  colors: [
-                                    Colors.black.withOpacity(0.88),
-                                    Colors.grey
-                                  ],
-                                ),
-                                borderRadius: BorderRadius.circular(100),
+                              margin: const EdgeInsets.only(right: 8),
+                              width: 40,
+                              height: 40,
+                              child: Image.asset(
+                                "assets/icons/canada.png",
                               ),
-                              child: Center(
-                                child: Icon(
-                                  Icons.power_settings_new_outlined,
-                                  color: Colors.white.withOpacity(0.6),
-                                  size: 40,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Canada",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold),
                                 ),
-                              ),
+                                Container(
+                                  child: Text(
+                                    "512.122.34.3",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w700,
+                                        color: Colors.black.withOpacity(0.6),
+                                        fontSize: 12),
+                                  ),
+                                )
+                              ],
                             )
                           ],
                         ),
-                      ),
+                        Container(
+                          child: const Icon(Icons.arrow_outward),
+                        )
+                      ],
                     ),
                   ),
-                ),
+                )
               ],
             ),
-          )
+          ),
         ],
       ),
     );
   }
 
+  Column _buildToggleBtn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 35),
+          height: 48,
+          width: 32,
+          child: SvgPicture.asset(
+            "assets/icons/arrow_up.svg",
+            fit: BoxFit.cover,
+          ),
+        ),
+        SlideTransition(
+          position: _switchAnimation,
+          child: GestureDetector(
+            onTap: () {
+              if (_switchAnimation.value == Offset.zero) {
+                _switchSlideController.forward();
+              } else {
+                _switchSlideController.reverse();
+              }
+            },
+            child: Container(
+              margin: const EdgeInsets.only(bottom: 8),
+              width: 108,
+              height: 158,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                gradient: const LinearGradient(
+                  begin: Alignment.topCenter,
+                  transform: GradientRotation(9.87),
+                  stops: [0.1, 1],
+                  colors: [
+                    Color.fromRGBO(178, 181, 54, 1),
+                    Color.fromRGBO(126, 177, 88, 1),
+                  ],
+                ),
+              ),
+              child: Container(
+                margin: const EdgeInsets.only(top: 40),
+                child: Column(
+                  children: [
+                    const Text(
+                      MyStrings.stop,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 16),
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          stops: const [0, 1],
+                          end: Alignment.bottomCenter,
+                          colors: [Colors.black.withOpacity(0.88), Colors.grey],
+                        ),
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.power_settings_new_outlined,
+                          color: Colors.white.withOpacity(0.6),
+                          size: 40,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   Widget _buildUploadDownloadSpeed(
-      String speed, SpeedTypeEnum speedType, UploadDownloadEnum status) {
+      String speed, String speedType, String status) {
     return Row(
       children: [
         RotatedBox(
-          quarterTurns: status.name == "download" ? 3 : 1,
+          quarterTurns: status == "download" ? 3 : 1,
           child: const Icon(
             Icons.arrow_back_ios_new_sharp,
             size: 18,
@@ -267,7 +346,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
             text: speed,
             children: <InlineSpan>[
               TextSpan(
-                text: speedType.name == "mb" ? " Mb/s" : " Kb/s",
+                text: speedType,
                 style: const TextStyle(
                   color: Colors.grey,
                   fontSize: 14,
